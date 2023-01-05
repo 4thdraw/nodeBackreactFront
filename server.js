283cde622d7179aa7a3898b75d2f10185f004ff1
4thdraw <4thdraw2@gmail.com>
2023-01-05 11:53:49 +0900

    프론트앤드 세팅완료

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors'); // 다른 서버와 연동호출
 const app = express(); // 실행해라 
 
 
+
 app.use(cors()); // cors 미들웨어를 삽입합니다. 다른 도메인에서 너한테 요청오면 응답해줘
 
 app.get('/', (req,res) => { // 요청패스에 대한 콜백함수를 넣어줍니다. get, post, use
